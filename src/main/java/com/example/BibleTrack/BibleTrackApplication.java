cf74bd33d154c80dcb1d83ee6083c1c003fca2ec
dribletv <alissonfsb14@hotmail.com>
2025-08-17 11:43:20 -0300

    initializing project
    Creating projetct with the dependencies and base skeleton

---
+++ src/main/java/com/example/BibleTrack/BibleTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.BibleTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BibleTrack/BibleTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BibleTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
